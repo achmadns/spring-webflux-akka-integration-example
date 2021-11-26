ce5c5671e5c78fbf90be4e75a1b652925880741c
Achmad Nasirudin Sandi <achmad.dev@gmail.com>
2021-11-26 10:50:55 +0700

    Initial commit.

---
+++ src/main/java/id/co/alamisharia/simjam/SimjamApplication.java
@@ -0,0 +1,13 @@
+package id.co.alamisharia.simjam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimjamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimjamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/co/alamisharia/simjam/SimjamApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.alamisharia.simjam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimjamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
